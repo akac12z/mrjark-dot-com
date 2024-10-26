6775c114b26fc44c0c0b56f1e65c73a21c7772f4
Chema <mrjark7@gmail.com>
2024-10-26 20:36:54 +0200

    añadido el adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: 'server',
+  adapter: vercel()
 });
