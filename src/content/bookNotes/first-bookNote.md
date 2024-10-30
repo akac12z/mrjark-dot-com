ddf4cd0dd0fe84b353d12c13b625fc98db4cffe5
Chema <mrjark7@gmail.com>
2024-10-30 13:55:45 +0100

    solución del problema con el schema. lo he comentado par aque no me de error

--- src/content/bookNotes/first-bookNote.md
+++ src/content/bookNotes/first-bookNote.md
@@ -1,21 +1,5 @@
 ---
 title: "Título del libro o nota" # (Máx. 60 caracteres)
-slug: "titulo-libro" # (URL amigable)
-date: "2024-09-24" # Fecha de creación o publicación
-author: "Autor del libro" # Autor del libro reseñado
-bookCover: "/images/book-cover.jpg" # Imagen de portada del libro
-bookCoverAlt: "Descripción accesible de la portada del libro" # Texto alternativo para la imagen del libro
-summary: "Resumen del libro o principales ideas" # Resumen breve (máx. 160 caracteres)
-tags: ["psicología", "hábitos"] # Lista de etiquetas relacionadas con el libro
-keyTakeaways: ["Punto clave 1", "Punto clave 2"] # Principales conclusiones del libro
-readingTime: "7 min read" # Estimación del tiempo de lectura de las notas
-metaTitle: "Título optimizado para SEO" # (si es diferente del `title`)
-metaDescription: "Descripción optimizada para SEO" # (Máx. 160 caracteres)
-ogImage: "/images/og-image-book.jpg" # Imagen Open Graph para redes sociales
-ogTitle: "Título para redes sociales" # (si es diferente del `title`)
-ogDescription: "Descripción para redes sociales" # Descripción para Open Graph
-twitterCard: "summary_large_image" # Tipo de tarjeta de Twitter
-keywords: ["resumen libro", "psicología", "hábitos"] # Palabras clave específicas para SEO
 ---
 
 ejemplo de un book notes
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,26 +1,30 @@
 import { defineCollection, z } from "astro:content";
 
+/**
+ * Las comento para que no me den errores
+ */
+
 const blogs = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(), 
-    createdDate: z.date(), 
-    lastTimeEdited: z.string().date().optional(), 
-    description: z.string(), 
-    tags: z.array(z.string()), 
-    blogImage: z.object({
-      src: z.union([z.string().url(), z.string()]), 
-      alt: z.string(),
-    }),
-    language: z.enum(["es"]),
-    author: z.string().default("mrjark"), 
-    authorLink: z.string(), 
-    readingTime: z.string(), 
-    categories: z.array(z.string()), 
-    keywords: z.array(z.string()), 
-    // slug: z.string(), // Slug para SEO
-    status: z.boolean().default(true), 
-    canonicalURL: z.string().url(),
+    // createdDate: z.date(), 
+    // lastTimeEdited: z.string().date().optional(), 
+    // description: z.string(), 
+    // tags: z.array(z.string()), 
+    // blogImage: z.object({
+    //   src: z.union([z.string().url(), z.string()]), 
+    //   alt: z.string(),
+    // }),
+    // language: z.enum(["es"]),
+    // author: z.string().default("mrjark"), 
+    // authorLink: z.string(), 
+    // readingTime: z.string(), 
+    // categories: z.array(z.string()), 
+    // keywords: z.array(z.string()), 
+    // // slug: z.string(), // Slug para SEO
+    // status: z.boolean().default(true), 
+    // canonicalURL: z.string().url(),
   }),
 });
 
@@ -28,27 +32,27 @@ const bookNotes = defineCollection({
   type: "content",
   schema: z.object({
     title: z.string(), 
-    createdDate: z.date(), 
-    lastTimeEdited: z.string().date().optional(), 
-    description: z.string(),
-    tags: z.array(z.string()), 
-    bookImage: z.object({
-      src: z.union([z.string().url(), z.string()]), 
-      alt: z.string(),
-    }),
-    language: z.enum(["es"]),
-    author: z.string().default("mrjark"), 
-    authorLink: z.string(), 
-    readingTime: z.string(), 
-    categories: z.array(z.string()), 
-    keywords: z.array(z.string()), 
-    // slug: z.string(), // Slug para SEO
-    status: z.boolean().default(true), 
-    canonicalURL: z.string().url(),
-    buyOnAmazon: z.object({
-      spain: z.string().url(),
-      usa: z.string().url()
-    }).optional()
+    // createdDate: z.date(), 
+    // lastTimeEdited: z.string().date().optional(), 
+    // description: z.string(),
+    // tags: z.array(z.string()), 
+    // bookImage: z.object({
+    //   src: z.union([z.string().url(), z.string()]), 
+    //   alt: z.string(),
+    // }),
+    // language: z.enum(["es"]),
+    // author: z.string().default("mrjark"), 
+    // authorLink: z.string(), 
+    // readingTime: z.string(), 
+    // categories: z.array(z.string()), 
+    // keywords: z.array(z.string()), 
+    // // slug: z.string(), // Slug para SEO
+    // status: z.boolean().default(true), 
+    // canonicalURL: z.string().url(),
+    // buyOnAmazon: z.object({
+    //   spain: z.string().url(),
+    //   usa: z.string().url()
+    // }).optional()
   }),
 });
 
