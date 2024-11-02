315398e08eef4ebe7959e6382502572115717ebd
Chema <mrjark7@gmail.com>
2024-11-02 13:04:10 +0100

    añadidos archivos para crear el custom color cuando estás en una ruta determinada

---
+++ src/lib/headerLinks.ts
@@ -0,0 +1,17 @@
+export const headerLinks = [
+  {
+    href: '/essay', 
+    text: 'Essays', 
+    customClasses: 'hover:border-b hover:border-mr-neon-pink hover:text-mr-neon-pink'
+  },
+  {
+    href: '/book-notes', 
+    text: 'Book Notes', 
+    customClasses: 'hover:border-b hover:border-mr-neon-gold hover:text-mr-neon-gold'
+  },
+  {
+    href: '/about', 
+    text: 'About', 
+    customClasses: 'hover:border-b hover:border-mr-neon-blue hover:text-mr-neon-blue'
+  },
+]
