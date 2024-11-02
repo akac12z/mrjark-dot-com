386c549f8346e3d8f8dcb9272703ee1e49292957
Chema <mrjark7@gmail.com>
2024-11-02 10:59:27 +0100

    He cambiado la configuración del output de server a hybrid (static aun no lo acepta vercel) porque tiene sentido que estén estáticas y porque la experiencia que tenía al cargar las pantallas era muy lenta además de eliminar las rutas dinánmicas que por ahora no me hacian falta como las del booksnote y la del blog las cuales me las estaba generando de forma estática

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,11 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig( {
   integrations: [ tailwind(), mdx() ],
-  output: 'server',
+  /*
+  Tiene sentido que sea hybrid porque las páginas que yo tengo son estáticas
+  y solo cuando sea necesario tendrá que hacer la carga de las mismas. 
+  En casos de APIs y demás, cosa que por ahora no
+  */
+  output: 'hybrid',
   adapter: vercel()
 } );
