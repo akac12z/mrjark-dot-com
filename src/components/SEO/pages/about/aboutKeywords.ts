e4d4fee95bd6880ca2c5cf20a746809275361428
Chema <mrjark7@gmail.com>
2024-11-12 11:02:23 +0100

    creando los files para los keywords

---
+++ src/components/SEO/pages/about/aboutKeywords.ts
@@ -0,0 +1,7 @@
+interface PageKeywords {
+  keywords: string[];
+}
+
+export const aboutKeywords: PageKeywords = {
+  keywords: ["mrjark", "mrJark", 'patatas jamon', 'Brochetas pollo', 'Brochetas Pollo']
+}
