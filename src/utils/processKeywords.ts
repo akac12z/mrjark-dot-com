d37e1e6f67155d64e66f912cd8c1f84e146ef660
Chema <mrjark7@gmail.com>
2024-11-18 14:00:11 +0100

    añadido al process el trim para espacios vacios y el regex para sustitución de estapacios vacios por guión medio

--- src/utils/processKeywords.ts
+++ src/utils/processKeywords.ts
@@ -1,7 +1,14 @@
 import type { PageKeywords } from "@interfaces/keywords.interface";
 
-
-export default function processKeywords(pageKeywords: PageKeywords): PageKeywords {
-  const uniqueKeywords = Array.from(new Set(pageKeywords.keywords.map(kw => kw.toLowerCase())));
+export default function processKeywords(
+  pageKeywords: PageKeywords
+): PageKeywords {
+  const uniqueKeywords = Array.from(
+    new Set(
+      pageKeywords.keywords.map((kw) =>
+        kw.toLowerCase().trim().replace(/\s+/g, "-")
+      )
+    )
+  );
   return { keywords: uniqueKeywords };
 }
