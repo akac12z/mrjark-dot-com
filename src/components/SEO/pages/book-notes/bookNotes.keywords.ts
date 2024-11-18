42af546ad8c042e9b1dacbc352466e36da9b2e3f
Chema <mrjark7@gmail.com>
2024-11-18 13:03:49 +0100

    modificación del book notes keywords y head

--- src/components/SEO/pages/book-notes/bookNotes.keywords.ts
+++ src/components/SEO/pages/book-notes/bookNotes.keywords.ts
@@ -1,6 +1,11 @@
 import type { PageKeywords } from "@interfaces/keywords.interface";
 
-
 export const bookNotesKeywords: PageKeywords = {
-  keywords: ["summary books" ] 
-}
+  keywords: [
+    "summary books",
+    "resúmenes de libros para startup",
+    "resúmenes de libros de growth",
+    "resúmenes de libros de crecimiento personal",
+    "resúmenes de libros de psicología del comportamiento",
+  ],
+};
