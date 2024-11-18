ff9be9143fa86019cf12ed3ff9e96f2b4dd1f3f9
Chema <mrjark7@gmail.com>
2024-11-18 13:04:08 +0100

    creado el seo básico de la página de los bias

---
+++ src/components/SEO/pages/bias/bias.keywords.ts
@@ -0,0 +1,18 @@
+import type { PageKeywords } from "@interfaces/keywords.interface";
+
+export const biasKeywords: PageKeywords = {
+  keywords: [
+    "Explicación y aplicación de los sesgos cognitivos",
+    "sesgos",
+    "bias",
+    "sesgos cognitivos",
+    "psicología del comportamiento",
+    "sesgos humanos",
+    "comportamiento humano",
+    "sesgos de comportamiento",
+    "sesgos aplicados a startups",
+    "sesgos aplicados a growth",
+    "sesgos aplicados",
+    "cómo funionan los sesgos humanos",
+  ],
+};
