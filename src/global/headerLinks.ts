287622fca8ba33697c62b25f5d5debcb7ff872f3
Chema <mrjark7@gmail.com>
2024-11-29 19:04:59 +0100

    cambio de "

--- src/global/headerLinks.ts
+++ src/global/headerLinks.ts
@@ -1,20 +1,19 @@
 import type { HeaderLinksInterface } from "@interfaces/headerLinks.interface";
 
-
 export const HEADER_LINKS: HeaderLinksInterface[] = [
   {
-    href: '/essay', 
-    text: 'Essays', 
-    customClasses: 'hover:text-mr-neon-pink'
+    href: "/essay",
+    text: "Essays",
+    customClasses: "hover:text-mr-neon-pink",
   },
   {
-    href: '/book-notes', 
-    text: 'Book Notes', 
-    customClasses: 'hover:text-mr-neon-gold'
+    href: "/book-notes",
+    text: "Book Notes",
+    customClasses: "hover:text-mr-neon-gold",
   },
   {
-    href: '/about', 
-    text: 'About', 
-    customClasses: 'hover:text-mr-neon-blue'
+    href: "/about",
+    text: "About",
+    customClasses: "hover:text-mr-neon-blue",
   },
-]
+];
