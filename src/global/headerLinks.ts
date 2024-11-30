57785c4e26b162de5c147cec26157478c82753b7
Chema <mrjark7@gmail.com>
2024-11-30 10:58:54 +0100

    intentander ver los fallos

--- src/global/headerLinks.ts
+++ src/global/headerLinks.ts
@@ -21,4 +21,9 @@ export const HEADER_LINKS: HeaderLinksInterface[] = [
     text: "About",
     customClasses: "hover:text-mr-neon-lime text-mr-neon-lime",
   },
+  {
+    href: "/testing",
+    text: "Testing",
+    customClasses: "hover:text-mr-neon-red text-mr-neon-red",
+  },
 ];
