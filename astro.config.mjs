7800e9ca069cea266507a381eca9e6b5d70b4fbe
Chema <mrjark7@gmail.com>
2025-01-19 11:36:41 +0100

    created env field

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -11,14 +11,26 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import partytown from '@astrojs/partytown';
+
 // Este es provisional hasta que Vercel arregle el problema con sitemap
 import { sitemapCopier } from './sitemap-copier';
+
 
 // https://astro.build/config
 export default defineConfig( {
   site: 'https://mrjark.com',
-  integrations: [ tailwind(), mdx(), sitemap(), react(), sitemapCopier() ],
+  integrations: [ tailwind(), mdx(), sitemap(), react(), sitemapCopier(), partytown( {
+    config: {
+      forward: [ 'dataLayer.push' ]
+    }
+  } ) ],
   output: 'static',
+  env: {
+    schema: {
+      GA4_MEASUREMENT_ID: envField.string( { context: 'server', access: 'secret', optional: false } )
+    }
+  },
   adapter: vercel( {
     webAnalytics: {
       enabled: true,
