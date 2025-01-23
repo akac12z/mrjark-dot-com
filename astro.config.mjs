5736699a61d6a4f98eeb4baf3129e3065a3fe37b
Chema <mrjark7@gmail.com>
2025-01-23 10:07:28 +0100

    fix: update GA4 and GTM measurement ID context and access levels to client and public

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,8 +28,8 @@ export default defineConfig( {
   output: 'static',
   env: {
     schema: {
-      GA4_MEASUREMENT_ID: envField.string( { context: 'server', access: 'secret', optional: false } ),
-      GTM_MEASUREMENT_ID: envField.string( { context: 'server', access: 'secret', optional: false } )
+      GA4_MEASUREMENT_ID: envField.string( { context: 'client', access: 'public', optional: false } ),
+      GTM_MEASUREMENT_ID: envField.string( { context: 'client', access: 'public', optional: false } )
     }
   },
   adapter: vercel( {
