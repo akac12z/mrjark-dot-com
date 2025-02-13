84417b2caa8dd3853205f1fc7eb8f3615775f092
Chema <mrjark7@gmail.com>
2025-02-13 19:37:21 +0100

    fix: update text and classes in navigation links and home page for clarity and consistency

--- src/global/headerLinks.ts
+++ src/global/headerLinks.ts
@@ -3,24 +3,24 @@ import type { HeaderLinksInterface } from "@interfaces/headerLinks.interface";
 export const HEADER_LINKS: HeaderLinksInterface[] = [
   {
     href: "/ensayos",
-    text: "Ensayos",
+    text: "/ensayos",
     customClasses: "hover:text-mr-neon-blue text-mr-neon-blue",
   },
   {
     href: "/biblioteca",
-    text: "Biblioteca",
+    text: "/biblioteca",
     customClasses: "hover:text-mr-neon-gold text-mr-neon-gold",
   },
   {
     href: "/comportamiento",
-    text: "Comportamiento",
+    text: "/comportamiento",
     customClasses: "hover:text-mr-neon-violet text-mr-neon-violet",
   },
-  {
-    href: "/contexto",
-    text: "Contexto",
-    customClasses: "hover:text-mr-primary-green text-mr-primary-green",
-  },
+  // {
+  //   href: "/contexto",
+  //   text: "Contexto",
+  //   customClasses: "hover:text-mr-primary-green text-mr-primary-green",
+  // },
   // {
   //   href: "/testing",
   //   text: "Testing",
